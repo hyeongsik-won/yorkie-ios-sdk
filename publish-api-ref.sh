5d9590385c50faecad2f0d96b97ed867caa5675d
hyeongsik-won <hyeongsik.won@navercorp.com>
2022-11-24 18:22:02 +0900

    temp

--- publish-api-ref.sh
+++ publish-api-ref.sh
@@ -1,14 +1,15 @@
 TAG=${1}
-HOST_URL="git@oss.navercorp.com:hyeongsik-won/playground.git"
-HOST_DIR="./doc-repo"
+HOST_URL="https://github.com/hyeongsik-won/yorkie-ios-sdk.git"
+API_REF_BRANCH=api-ref-doc
+CLONED_DIR="./doc-repo"
 HOSTING_BASE_PATH="hyeongsik-won/playground"
 OUTPUT_SUB_PATH="docs"
-OUTPUT_PATH="${HOST_DIR}/${OUTPUT_SUB_PATH}"
+OUTPUT_PATH="${CLONED_DIR}/${OUTPUT_SUB_PATH}"
 
-rm -rf $HOST_DIR
-git clone $HOST_URL $HOST_DIR
-cd $HOST_DIR
-git checkout master
+rm -rf $CLONED_DIR
+git clone $HOST_URL $CLONED_DIR
+cd $CLONED_DIR
+git checkout $API_REF_BRANCH
 cd ..
 ls
 rm -rf $OUTPUT_PATH/*
@@ -18,9 +19,9 @@ swift package --allow-writing-to-directory $OUTPUT_PATH \
     --output-path $OUTPUT_PATH \
     --transform-for-static-hosting \
     --hosting-base-path $HOSTING_BASE_PATH
-cd $HOST_DIR
+cd $CLONED_DIR
 git add $OUTPUT_SUB_PATH
 git commit -m "Update API reference for version ${TAG}"
 git push
-rm -rf $HOST_DIR
+rm -rf $CLONED_DIR
 
