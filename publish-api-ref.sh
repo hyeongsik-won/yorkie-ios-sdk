6981710ab985913c775e9cdfcb29651c8fc67222
hyeongsik-won <hyeongsik.won@navercorp.com>
2022-11-24 21:45:15 +0900

    git checkout 안받게 수정

--- publish-api-ref.sh
+++ publish-api-ref.sh
@@ -1,17 +1,16 @@
 TAG=${1}
-HOST_URL="https://github.com/hyeongsik-won/yorkie-ios-sdk.git"
+#HOST_URL="https://github.com/hyeongsik-won/yorkie-ios-sdk.git"
 API_REF_BRANCH=api-ref-doc
-CLONED_DIR="./doc-repo"
+CLONED_DIR="doc-repo"
 HOSTING_BASE_PATH="hyeongsik-won/playground"
 OUTPUT_SUB_PATH="docs"
 OUTPUT_PATH="${CLONED_DIR}/${OUTPUT_SUB_PATH}"
 
-rm -rf $CLONED_DIR
-git clone $HOST_URL $CLONED_DIR
-cd $CLONED_DIR
-git checkout $API_REF_BRANCH
-cd ..
-ls
+#rm -rf $CLONED_DIR
+#git clone $HOST_URL $CLONED_DIR
+#cd $CLONED_DIR
+#git checkout $API_REF_BRANCH
+#cd ..
 rm -rf $OUTPUT_PATH/*
 swift package --allow-writing-to-directory $OUTPUT_PATH \
     generate-documentation --target Yorkie \
@@ -19,9 +18,12 @@ swift package --allow-writing-to-directory $OUTPUT_PATH \
     --output-path $OUTPUT_PATH \
     --transform-for-static-hosting \
     --hosting-base-path $HOSTING_BASE_PATH
-cd $CLONED_DIR
-git add $OUTPUT_SUB_PATH
+#cd $CLONED_DIR
+git checkout $API_REF_BRANCH
+rm -rf docs
+mv $OUTPUT_PATH docs
+#git add $OUTPUT_SUB_PATH
+git add docs
 git commit -m "Update API reference for version ${TAG}"
 git push
-rm -rf $CLONED_DIR
-
+#rm -rf $CLONED_DIR
